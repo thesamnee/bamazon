4b06e1dd81728ef1d74c80b971734f6e99196a44
Sam Nee <samuel.r.nee@gmail.com>
2019-12-11 19:43:01 -0500

    Challenge 1 complete

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# bamazon
+Bamazon is a marketplace much like Amazon, except this online marketplace doesn't ever send you the products you order. 
---
+++ app.js
@@ -0,0 +1,87 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+console.log(`
+WELCOME TO BAMAZON
+`)
+const connection = mysql.createConnection({
+    host: "localhost",
+    
+    port: 3306,
+    
+    user: "root",
+    
+    password: "Defsam1999",
+    database: "bamazon"
+});
+function showStock(){
+  connection.query("SELECT * FROM products", function(err, res){
+    console.log(`
+    ------------
+    CURRENT STOCK
+    ------------
+    `)
+    for (let i=0; i<res.length; i++){
+      console.log(`
+      |  ${res[i].id}  |  ${res[i].product_name}  |  \$${res[i].price}  |
+      `)
+    }
+  })
+}
+function userPrompt(){
+  inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "productChoice",
+            message: "What would you like to purchase? [INPUT ID]"
+        },
+        {
+            type: "input",
+            name: "quantChoice",
+            message: "How many would you like to purchase?"
+        }
+    ]).then(function(answers){
+        connection.query("SELECT * FROM products WHERE ?", { id: answers.productChoice }, function(err, res) {
+            if (!answers.productChoice || !answers.quantChoice){
+                console.log(`
+                PLEASE INPUT A VALUE
+                `)
+            }
+            if (res[0].stock - answers.quantChoice >= 0){
+                console.log(`
+                
+                We'll ship that to you ASAP!
+                
+                `)
+                connection.query("UPDATE products SET ? WHERE ?", [
+                    {
+                        stock: res[0].stock -= answers.quantChoice
+                    },
+                    {
+                        id: answers.productChoice
+                    }
+                ], function(error){
+                    if (error) throw error;
+                });
+                start();
+            } else {
+                console.log(`
+                
+                We don't have that many! **${res[0].stock} units left**
+                
+                `)
+                start();
+            };
+        });
+    });
+};
+
+connection.connect(function(err) {
+    if (err) throw err;
+});
+function start(){
+showStock();
+userPrompt();
+}
+start()
